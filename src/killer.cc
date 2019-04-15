650f4ccc4ba816a1fdc285d9339b4dadaf0ac42a
deckerling <46498067+deckerling@users.noreply.github.com>
2019-04-15 12:37:34 +0200

    Update killer.cc

--- src/killer.cc
+++ src/killer.cc
@@ -22,7 +22,7 @@
 
 #include "function_word_vector_killer.h"
 
-Killer::Killer(const std::string &input_file, const std::string &output_file, const std::vector<bool> &words_to_remove, const int language)
+Killer::Killer(const std::string& input_file, const std::string& output_file, const std::vector<bool>& words_to_remove, const int language)
     : input_file_(input_file),
       output_file_(output_file),
       words_to_remove_(words_to_remove),
@@ -45,27 +45,22 @@ void Killer::SelectAndRemoveWords() {
       if (!list_of_words_to_remove.empty()) {
         // Prints the "words_to_remove_" in the same order as they can be found
         // in their txt-file.
-        for (unsigned j = 0; j < list_of_words_to_remove.size(); ++j) {
-          std::cout << list_of_words_to_remove[j];
-          if (j != (list_of_words_to_remove.size()-1))
-            std::cout << ", ";
-        }
-        std::cout << '\n';
+        for (auto it = list_of_words_to_remove.begin(); it != list_of_words_to_remove.end(); ++it)
+          std::cout << *it << ((it != std::prev(list_of_words_to_remove.end()))? ", " : "\n");
       }
       // Shuffles the vector "list_of_words_to_remove" in order to improve the
       // binary tree that is about to be built (the lists of function words in
       // the directory "data" are sorted in alphabetical order (except for the
       // numerals) to make it easier for the user to find a certain word in
       // those files).
-      std::shuffle(std::begin(list_of_words_to_remove), std::end(list_of_words_to_remove), rng);
-      for (unsigned j = 0; j < list_of_words_to_remove.size(); ++j)
-        function_words_to_remove.AddWord(list_of_words_to_remove[j]); // adds the selected functions words to the binary tree
+      std::shuffle(list_of_words_to_remove.begin(), list_of_words_to_remove.end(), rng);
+      for (auto& word_to_remove : list_of_words_to_remove)
+        function_words_to_remove.AddWord(word_to_remove); // adds the selected functions words to the binary tree
     }
   }
   std::cout << "\n\tCreating new file (\"" << output_file_ << "\") without those words and their vectors..." << std::endl;
   int num_of_removed_vectors = 0, num_of_checked_vectors = 0, flush_interval_factor = 1;
-  std::string line;
-  std::string word;
+  std::string line, word;
   std::ifstream input_file_stream(input_file_);
   std::ofstream output_file_stream(output_file_);
   // Reads "input_file_", checks for every line if the word vector should be
@@ -97,64 +92,60 @@ std::vector<std::string> Killer::LoadFunctionWords(const int index) {
 // Loads the function words via index (the function words should be saved in
 // txt-files (check the paths!)).
   const std::string directory_to_open = (language_ == 0)? "data/english" : "data/german";
-  std::string file_to_open;
-  std::string line;
+  std::string file_to_open, line;
   switch (index) {
-    case 0:
-      file_to_open = "adpositions.txt";
-      break;
-    case 1:
-      file_to_open = "articles_and_the_like.txt";
-      break;
-    case 2:
-      file_to_open = "conjunctions_and_subjunctions.txt";
-      break;
-    case 3:
-      file_to_open = "interjections.txt";
-      break;
-    case 4:
-      file_to_open = "interrogative_words.txt";
-      break;
-    case 5:
-      file_to_open = "numerals.txt";
-      break;
-    case 6:
-      file_to_open = "particles.txt";
-      break;
-    case 7:
-      file_to_open = "personal_pronouns_and_possessives.txt";
-      break;
-    case 8:
-      file_to_open = "temporal_words.txt";
-      break;
-    case 9:
-      file_to_open = "miscellaneous_words.txt";
-      break;
+   case 0:
+    file_to_open = "adpositions.txt";
+    break;
+   case 1:
+    file_to_open = "articles_and_the_like.txt";
+    break;
+   case 2:
+    file_to_open = "conjunctions_and_subjunctions.txt";
+    break;
+   case 3:
+    file_to_open = "interjections.txt";
+    break;
+   case 4:
+    file_to_open = "interrogative_words.txt";
+    break;
+   case 5:
+    file_to_open = "numerals.txt";
+    break;
+   case 6:
+    file_to_open = "particles.txt";
+    break;
+   case 7:
+    file_to_open = "personal_pronouns_and_possessives.txt";
+    break;
+   case 8:
+    file_to_open = "temporal_words.txt";
+    break;
+   case 9:
+    file_to_open = "miscellaneous_words.txt";
   }
-  if (!CheckFile(directory_to_open+"/"+file_to_open)) {
+  if (!FileIsValid(directory_to_open+"/"+file_to_open)) {
     std::cerr << "\tThose function words couldn't be added to the words to remove.\n";
     return {}; // returns an empty vector if the file is bad or couldn't be found
   } else {
     std::ifstream file_stream(directory_to_open+"/"+file_to_open);
-    std::vector<std::string> list_of_words_to_remove;
-    while (std::getline(file_stream, line))
-      list_of_words_to_remove = GetWordsFromALine(line);
-    return list_of_words_to_remove;
+    std::getline(file_stream, line);
+    return GetWordsFromALine(line);
   }
 }
 
-std::vector<std::string> Killer::GetWordsFromALine(const std::string &line) {
+std::vector<std::string> Killer::GetWordsFromALine(const std::string& line) {
 // Returns a std::vector containing all words found in a line (every line will
 // be equal to the content of a function word file).
   std::stringstream stream(line);
   std::string word;
   std::vector<std::string> words;
-  while (getline(stream, word, ' '))
+  while (std::getline(stream, word, ' '))
     words.push_back(word);
   return words;
 }
 
-bool Killer::IsNumber(const std::string &word) {
+bool Killer::IsNumber(const std::string& word) {
 // Returns "true" if "word" (i.e. a string) is a number and "false" otherwise.
   int fractional_count = 0;
   for (unsigned i = 0; i < word.length(); ++i) {
